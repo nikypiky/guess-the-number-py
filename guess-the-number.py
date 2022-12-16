3db0c19a9056e908231f4f7b94ab73b917ed745f
Niky Piky <naldorsc2@gmail.com>
2022-12-17 00:09:36 +0100

    finishing touches

--- guess-the-number.py
+++ guess-the-number.py
@@ -4,8 +4,7 @@ random_number = random.randint(1, 20)
 
 name = 'name' #input('Hello, what is your name? ')
 print('Hello ' + name + ', this is a game of guess the number. I will think a number, and you od your best to find it out.')
-print(random_number)
-print(type(random_number))
+
 for i in range(5,-1,-1):
     
     number = input('Choose a number: ')
@@ -14,18 +13,19 @@ for i in range(5,-1,-1):
         print()
         continue
 
-    if int(number) < int(random_number):
+    if int(number) < random_number:
         if i == 1:
             print('The number', number, 'is not correct, mine is higher! You have', int(i), 'move left.')
         else:
             print('The number', number, 'is not correct, mine is higher! You have', int(i), 'moves left.')
-    elif int(number) > int(random_number):
+    elif int(number) > random_number:
         if i == 1:
             print('The number', number, 'is not correct, mine is lower! You have', int(i), 'move left.')
         else:
             print('The number', number, 'is not correct, mine is lower! You have', int(i), 'moves left.')
     else:
         print('That is correct!')
+        print()
         break
     if i == 0:
         print('Sorry looser!')
